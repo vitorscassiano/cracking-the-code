3687dd7821a72f63b61df0f012d673d6ef5904da
Vitor Cassiano <vitor.cassiano@olxbr.com>
2018-04-02 16:15:19 -0300

    Updates on exercism1

--- chapter1/exercism1.js
+++ chapter1/exercism1.js
@@ -18,11 +18,58 @@ function uniqueString(string){
     return unique;
 }
 
+function isUnique(word){
+    let a = [];
+    for(char of word) {
+        if(a[char]) return false;
+        a[char] = 1;
+    }
+
+    return true;
+}
+
+function isUniqueAscii(word){
+    let a = [];
+    for(w in word){
+        let charCode = word.charCodeAt(w);
+        if(a[charCode] > 0) return false;
+        a[charCode] = 1;
+    }
+    return true;
+}
+
+function isUniqueChars(word) {
+    let checker = 0;
+    for (i in word){
+        let val = word[i] - 'a';
+        console.log(val);
+        console.log(word[i]);
+        if((checker & (1 << val)) > 0) return false;
+        checker |= (1 << val);
+    }
+    return true;
+}
+
 function main(){
-    console.log(uniqueString('Implement'));
-    console.log(uniqueString('determine'));
+    console.time('hash');
     console.log(uniqueString('characters'));
     console.log(uniqueString('abcdefghijlmnopqrstuvxz'));
+    console.timeEnd('hash');
+
+    console.time('array');
+    console.log(isUnique('characters'));
+    console.log(isUnique('abcdefghijlmnopqrstuvxz'));
+    console.timeEnd('array');
+
+    //console.time('bitwise');
+    //console.log(isUniqueChars('characters'));
+    //console.log(isUniqueChars('abcdefghijlmnopqrstuvxz'));
+    //console.timeEnd('bitwise');
+
+    console.time('charcode')
+    console.log(isUnique("characters"));
+    console.log(isUnique("abcdefghijlmnopqrstuvxz"));
+    console.timeEnd('charcode')
 }
 
 main();
